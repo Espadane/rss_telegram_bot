10c5c28ecf21b8a9396638d29791ee4fe56ddecf
Espadane <espadane@gmail.com>
2022-02-05 17:54:41 +0300

    bug fix

--- vk.py
+++ vk.py
@@ -12,27 +12,31 @@ def get_posts_from_vk(request_url):
         api_request = f'https://api.vk.com/method/wall.get?domain={group_addr}&count=5&access_token={config.vk_token}&v=5.131'
     response = requests.get(api_request)
     api_data = response.json()
-    entries = api_data['response']['items']
-    posts = []
-    for entrie in entries[0:5]:
-        if entrie.get('is_pinned') != None:
-            continue
-        post_id = str(entrie['id'])
-        post_text = entrie['text']
-        if post_text == '':
-            post_text = 'Запись'
-        post_title_chunks = post_text.split(' ')
-        post_title = ' '.join(post_title_chunks[0:7]) + ' ...'
-        post_owner_id = str(entrie['owner_id'])
-        post_link = f'https://vk.com/id{post_owner_id.replace("-","")}?w=wall{post_owner_id}_{post_id}'
-        group_name = get_group_name(request_url)
-        posts.append({
-            'feed_name': group_name,
-            'feed_url': request_url,
-            'record_id': group_name + '_' + post_id,
-            'record_title': post_title,
-            'record_link': post_link
-        })
+    try:
+        entries = api_data['response']['items']
+        posts = []
+        for entrie in entries[0:5]:
+            if entrie.get('is_pinned') != None:
+                continue
+            post_id = str(entrie['id'])
+            post_text = entrie['text']
+            if post_text == '':
+                post_text = 'Запись'
+            post_title_chunks = post_text.split(' ')
+            post_title = ' '.join(post_title_chunks[0:7]) + ' ...'
+            post_owner_id = str(entrie['owner_id'])
+            post_link = f'https://vk.com/id{post_owner_id.replace("-","")}?w=wall{post_owner_id}_{post_id}'
+            group_name = get_group_name(request_url)
+            posts.append({
+                'feed_name': group_name,
+                'feed_url': request_url,
+                'record_id': group_name + '_' + post_id,
+                'record_title': post_title,
+                'record_link': post_link
+            })
+    except Exception as e:
+        print(e)
+
     return posts
 
 
