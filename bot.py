0ce5efcbd4b6f2fc33f9c68841d11a261a308c45
Espadane <52450374+Espadane@users.noreply.github.com>
2022-02-24 18:29:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,7 +144,7 @@ async def check_new_records(user_id):
                             else:
                                 await bot.send_message(user_id, f'#{feed_name}\n\n<a href="{link}">{title}</a>\n', parse_mode='HTML')
 
-        await asyncio.sleep(300)
+        await asyncio.sleep(1200)
 
 
 def create_loops():
